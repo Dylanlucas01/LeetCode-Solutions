53ee24f3e69637fca21617d5d56c22ba2b00f228
Dylan Lucas <58743412+Dylanlucas01@users.noreply.github.com>
2026-07-16 14:10:35 -0700

    Time: 55 ms (42.01%) | Memory: 29.1 MB (7.32%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,31 +1,31 @@
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         lowest_buy_price = prices[0]
-#         max_profit = 0
-
-#         for current_price in prices[1:]:
-#             if(current_price < lowest_buy_price):
-#                 lowest_buy_price = current_price
-            
-#             if(current_price > lowest_buy_price):
-#                 profit_today = current_price - lowest_buy_price
-#                 max_profit = max(profit_today, max_profit)
-
-#         return max_profit
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         lowest_buy_price = prices[0]
         max_profit = 0
 
         for current_price in prices[1:]:
-            lowest_buy_price = min(lowest_buy_price, current_price)
-
-            profit_today = current_price - lowest_buy_price
-
-            max_profit = max(max_profit, profit_today)
+            if(current_price < lowest_buy_price):
+                lowest_buy_price = current_price
+            
+            if(current_price > lowest_buy_price):
+                profit_today = current_price - lowest_buy_price
+                max_profit = max(profit_today, max_profit)
 
         return max_profit
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         lowest_buy_price = prices[0]
+#         max_profit = 0
+
+#         for current_price in prices[1:]:
+#             lowest_buy_price = min(lowest_buy_price, current_price)
+
+#             profit_today = current_price - lowest_buy_price
+
+#             max_profit = max(max_profit, profit_today)
+
+#         return max_profit
 
 # class Solution:
 #     def maxProfit(self, prices: List[int]) -> int:
