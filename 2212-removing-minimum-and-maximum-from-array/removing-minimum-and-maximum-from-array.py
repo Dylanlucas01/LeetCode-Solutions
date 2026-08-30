6f705e366951fd91f10192069049ab59d94e7847
Dylan Lucas <58743412+Dylanlucas01@users.noreply.github.com>
2026-08-30 12:49:12 -0700

    Time: 19 ms (65.68%) | Memory: 33.6 MB (35.50%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
@@ -1,61 +1,20 @@
 class Solution:
     def minimumDeletions(self, nums: List[int]) -> int:
-        high = max(nums)
-        high_index = nums.index(high)
 
-        low = min(nums)
-        low_index = nums.index(low)
+        nums_length = len(nums)
+        
+        high_index = nums.index(max(nums))
+        low_index = nums.index(min(nums))
 
         left = min(low_index, high_index)
         right = max(low_index, high_index)
 
         delete_front = left + 1
-        delete_back = len(nums) - right
+        delete_back = nums_length - right
         delete_f_b = delete_front + delete_back
 
         delete_left = right + 1
 
-        delete_right = len(nums) - left
+        delete_right = nums_length - left
 
         return min(delete_left, delete_right, delete_f_b)
-
-
-
-
-
-
-
-
-
-        
-
-        # | LB <------- LI = 2 (Delete Front)
-        # | RI -------> RB = 3 (Delete Back)
-
-        # | LB <------- RI = 6 (Delete Left)
-        
-        # | LI -------> RB = 7 (Delete Right)
-
-
-        # remove left + right
-        # nums = [2,|10|,7,5,4,|1|,8,6]
-        # nums = [0,| 1|,2,3,4,|5|,6,7]
-
-        # 0 , 1 , 5 , 7
-
-        # find distance from right bound to first index
-        # find distance from left bound to second index
-        # find distance between both indexs
-
-        # remove left
-        # nums = [0,|-4|,|19|,1,8,-2,-3,5]
-        # nums = [0,| 1|,| 2|,3,4, 5, 6,7]
-
-        # 0 , 1 , 2 , 7
-
-        # remove right
-        # nums = [0,1,8,-2,-3,|-4|,|19|,5]
-        # nums = [0,1,2, 3, 4,| 5|,| 6|,7]
-
-        # 0 , 5 , 6 , 7
-        
