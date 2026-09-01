eabf3098d21dfca294336b2b0dc0e7a7ed379db5
Dylan Lucas <58743412+Dylanlucas01@users.noreply.github.com>
2026-08-31 22:27:14 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (62.47%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
@@ -5,10 +5,9 @@ class Solution:
         digit_product = 1
 
         while m > 0:
-            digit = m % 10
+            digit_sum += m % 10
+            digit_product *= m % 10
             m = m // 10
-            digit_sum += digit
-            digit_product *= digit
 
         return n % (digit_sum + digit_product) == 0
         
